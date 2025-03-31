8b2dafc90da50b8fa272a777fa851279b15b642b
Vedang <vedang.wappnet@gmail.com>
2025-03-31 15:30:32 +0530

    main file is updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
+dotenv.config()
+//console.log('JWT_SECRET:', process.env.JWT_SECRET);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  app.enableCors();
+  
+  // serving static files from uploads directory
+  await app.listen(process.env.PORT ?? 8000);
+  console.log('🚀Server is running on port: 8000')
 }
 bootstrap();
