8617c2c9ee514525939e6c6e5875e64a7032e47c
Vedang <vedang.wappnet@gmail.com>
2025-03-31 15:19:47 +0530

    user entity is created

---
+++ backend/src/user/entities/users.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from "typeorm";
+
+@Entity({ name: 'user' })
+export class User{
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    username: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+    
+    
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
