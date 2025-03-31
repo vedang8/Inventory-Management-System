be7b6fda598b25a1aaab56279cebeb5eec679c06
Vedang <vedang.wappnet@gmail.com>
2025-03-31 15:31:40 +0530

    chnages are done

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/modules/users.module';
+import { AuthModule } from './auth/modules/auth.module';
+import { typeOrmConfig } from './config/database.config';
+import { AuthMiddleware } from './auth/middlewares/auth.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    UsersModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(AuthMiddleware)
+    .forRoutes(
+      { path: 'users', method: RequestMethod.ALL },
+    );
+  }
+}
